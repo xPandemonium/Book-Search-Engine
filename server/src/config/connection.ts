26805e511ed5840bed271bd8d313b2bdf30f1469
Pandemonium <jonathancorreaiglesias@gmail.com>
2025-05-06 03:13:25 -0500

    connection to db bug fix

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
 
-export default mongoose.connection;
+const db = mongoose.connection;
+
+export default db;
